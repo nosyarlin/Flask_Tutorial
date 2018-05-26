bc0f49c37c43e6c0f2d0fd2b897c99eb77468da4
Rayson Lim Jun Kai <rayson@jublia.com>
2018-05-26 15:05:37 +0800

    first commit

---
+++ code/resources/items.py
@@ -0,0 +1,72 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models import ItemModel
+
+
+class Item(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="Price field cannot be left empty"
+    )
+    parser.add_argument(
+        'store_id',
+        type=int,
+        required=True,
+        help="Store_id field cannot be left empty"
+    )
+
+    @jwt_required()
+    def get(self, name):
+        try:
+            item = ItemModel.find_by_name(name)
+        except Exception:
+            return {'message': 'An error occurred at retriving item'}, 500
+
+        if item:
+            return item.json(), 200
+        return {'message': 'Item not found'}, 404
+
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {'message': 'An item with name %s already exists' % (name)}
+
+        data = Item.parser.parse_args()
+        item = ItemModel(name, **data)
+
+        try:
+            item.save_to_db()
+        except Exception:
+            return {'message': 'An error occurred at insertion'}, 500
+
+        return item.json(), 201
+
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+
+        return {'message': 'Item deleted'}, 200
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+
+        if item is None:
+            item = ItemModel(name, **data)
+        else:
+            item.price = data['price']
+            item.store_id = data['store_id']
+
+        item.save_to_db()
+
+        return item.json(), 200
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': [item.json() for item in ItemModel.query.all()]}, 200
